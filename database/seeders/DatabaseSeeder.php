a69e9f781377f80f55b5f3c514e3d282a11ed046
MiDo <mohamedkarabawy@hotmail.com>
2025-03-21 06:47:30 +0200

    Demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Admin::create([
+        User::create([
             'name' => 'Admin',
             'role' => 'admin',
             'email' => 'admin@admin.cc',
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/login', function () {
+    return view('index');
+});
+
+Route::get('/signup', function () {
+    return view('index');
+});
+
+Route::get('/users', function () {
+    return view('index');
+});
+
+Route::get('/events', function () {
+    return view('index');
+});
+
+Route::get('/events/{id}/attendance', function () {
+    return view('index');
+});
+
+Route::get('/attendee-events', function () {
+    return view('index');
 });
